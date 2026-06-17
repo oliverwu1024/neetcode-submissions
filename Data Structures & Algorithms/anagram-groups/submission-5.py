582ce8fc928b503b54da5719643373f43874e06d
oliverwu1024 <153962603+oliverwu1024@users.noreply.github.com>
2026-06-17 12:17:49 +1000

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.py
@@ -0,0 +1,8 @@
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        store = defaultdict(list)
+        for i in strs:
+            store["".join(sorted(i))].append(i)
+        
+        return [x for x in store.values()]
